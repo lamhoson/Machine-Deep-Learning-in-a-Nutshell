ab0c1dd72507496720bd30520aef39b623148dde
Big Horse <34258914+lamhoson@users.noreply.github.com>
2020-12-23 18:52:37 +0800

    update

---
+++ dense.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Softmax
+mode=Sequential()
+
+mode.add([Dense(64), input_shape=(64,64)])
+
+
+
+
